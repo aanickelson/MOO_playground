3028222b879452408c02374543ee9ac5c3f041a6
aanickelson <aanickelson@gmail.com>
2023-01-16 10:58:58 -0800

    Added option to use a single-layer perceptron. This seems to have solved many of the search issues we were having.
    Next step is to clean up the code and change the way data is handled so we can run multiple stat runs and do so in parallel.

--- problem_wrapper.py
+++ problem_wrapper.py
@@ -1,13 +1,13 @@
 import numpy as np
 from pymoo.core.problem import ElementwiseProblem
 from teaming.domain import DiscreteRoverDomain
-from MOO_playground.learning.neuralnet import NeuralNetwork as NN
+from MOO_playground.learning.neuralnet_no_hid import NeuralNetwork as NN
 from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.optimize import minimize
 from pymoo.visualization.scatter import Scatter
 from torch import from_numpy
 
-from parameters010 import Parameters as p
+from parameters02 import Parameters as p
 
 
 class RoverWrapper(ElementwiseProblem):
@@ -16,19 +16,19 @@ class RoverWrapper(ElementwiseProblem):
         self.st_size = env.state_size()
         self.hid = env.p.hid
         self.act_size = env.get_action_size()
-        self.l1_size = self.st_size * self.hid
+        self.l1_size = self.st_size * self.act_size
         self.l2_size = self.hid * self.act_size
         self.model = NN(self.st_size, self.hid, self.act_size)
         self.last_two_0 = [0, 0]
         self.last_two_1 = [0, 0]
         self.gen = 0
-        super().__init__(n_var=self.l1_size + self.l2_size, n_obj=2, n_ieq_constr=0, xl=-5, xu=5)
+        super().__init__(n_var=self.l1_size, n_obj=2, n_ieq_constr=0, xl=-5, xu=5)
 
     def _evaluate(self, x, out, *args, **kwargs):
         self.env.reset()
-        l1_wts = from_numpy(np.reshape(x[:self.l1_size], (self.hid, self.st_size)))
-        l2_wts = from_numpy(np.reshape(x[self.l1_size:], (self.act_size, self.hid)))
-        self.model.set_weights([l1_wts, l2_wts])
+        l1_wts = from_numpy(np.reshape(x[:self.l1_size], (self.act_size, self.st_size)))
+        # l2_wts = from_numpy(np.reshape(x[self.l1_size:], (self.act_size, self.hid)))
+        self.model.set_weights([l1_wts])
         self.env.run_sim([self.model])
         out["F"] = -self.env.multiG()
         # print(out['F'])
@@ -44,8 +44,10 @@ if __name__ == '__main__':
     env = DiscreteRoverDomain(p)
     problem = RoverWrapper(env)
     algorithm = NSGA2(pop_size=200)
-    res = minimize(problem, algorithm, ('n_gen', 200))
+    res = minimize(problem, algorithm, ('n_gen', 500))
     print(-res.F)
+    # NN weights are stored in res.X
+    # print(res.X)
     # print(problem.last_two_0, problem.last_two_1)
     plot = Scatter()
     plot.add(problem.pareto_front(), plot_type="line", color="black", alpha=0.7)
